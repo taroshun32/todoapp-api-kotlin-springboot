e41f74a75a387c14605107fe9d7dd9f8b6318c97
“taroshun32” <styk.hd.2738@gmail.com>
2020-04-25 05:27:59 +0900

    add environments

---
+++ src/main/kotlin/com/example/todo/config/Environments.kt
@@ -0,0 +1,53 @@
+package com.example.todo.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+/**
+ * application.ymlから設定値を取得するクラス
+ * environments以下の項目を取得する
+ */
+@ConstructorBinding
+@ConfigurationProperties(prefix = "environments")
+data class Environments(
+  /** 環境プロファイル */
+  val profile: String
+) {
+
+  /** 各環境ごとのプロファイル名 */
+  companion object {
+    private const val PROFILE_PRD = "prd"
+    private const val PROFILE_STG = "stg"
+    private const val PROFILE_DEV = "dev"
+    private const val PROFILE_LOCAL = "local"
+  }
+
+  enum class Profile {
+    PRD, STG, DEV, LOCAL;
+  }
+
+  fun profile(): Profile {
+    return when (profile) {
+      PROFILE_PRD -> Profile.PRD
+      PROFILE_STG -> Profile.STG
+      PROFILE_DEV -> Profile.DEV
+      PROFILE_LOCAL -> Profile.LOCAL
+      else -> Profile.LOCAL // TODO 例外作成時に対応
+    }
+  }
+
+  /**
+   * 本番環境かどうかを判断するメソッド
+   */
+  val isPrd
+    get() = profile == PROFILE_PRD
+
+  val isStg
+    get() = profile == PROFILE_STG
+
+  val isDev
+    get() = profile == PROFILE_DEV
+
+  val isLocal
+    get() = profile == PROFILE_LOCAL
+}
