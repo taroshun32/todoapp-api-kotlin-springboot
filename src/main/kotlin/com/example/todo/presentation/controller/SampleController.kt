1911d65a8d6db9cf8e810d3b64d948b4fbb3c8fc
“taroshun32” <styk.hd.2738@gmail.com>
2020-05-13 23:45:07 +0900

    update init_table.sql

--- src/main/kotlin/com/example/todo/presentation/controller/SampleController.kt
+++ src/main/kotlin/com/example/todo/presentation/controller/SampleController.kt
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/sample")
-class SampleApiController() {
+@RequestMapping("/sample")
+class SampleController() {
 
   @GetMapping
   fun ok(): HttpStatus = HttpStatus.OK
